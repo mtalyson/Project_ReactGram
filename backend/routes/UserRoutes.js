368dc3dddc8562dbd85f89e75bb1d0a1a1cd3513
Talyson Moreira <talyson.penha@gmail.com>
2022-09-30 18:12:48 -0300

    auth middleware and logged user controller

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,14 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 // user controller
-const { register, login } = require("../controllers/UserController")
+const { register, login, getCurrentUser } = require("../controllers/UserController")
 
 // middlewares
 const validate = require("../middlewares/handleValidation")
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
+const authGuard = require("../middlewares/authGuard")
 
 // user routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router
