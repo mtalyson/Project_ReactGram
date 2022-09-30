c1309c4df1da42589da9c96a1b55e125db332dd6
Talyson Moreira <talyson.penha@gmail.com>
2022-09-30 16:59:14 -0300

    validation middleware created

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router()
 // user controller
 const { register } = require("../controllers/UserController")
 
+// middlewares
+const validate = require("../middlewares/handleValidation")
+
 // user routes
-router.post("/register", register)
+router.post("/register", validate, register)
 
 module.exports = router
