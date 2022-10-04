56ba7d7cc6350d8e7068e0f95450ea89088df5d5
Talyson Moreira <talyson.penha@gmail.com>
2022-10-04 20:09:16 -0300

    get all photos controller and route created

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -66,7 +66,14 @@ const deletePhoto = async (req, res) => {
   
 }
 
+// get all photos controller
+const getAllPhotos = async (req, res) => {
+  const photos = await Photo.find({}).sort([["createdAt", -1]]).exec()
+  return res.status(200).json(photos)
+}
+
 module.exports = {
   insertPhoto,
   deletePhoto,
+  getAllPhotos,
 }
