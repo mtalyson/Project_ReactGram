094716f4489c1fa0c561ce8759f439f7f113691e
Talyson Moreira <talyson.penha@gmail.com>
2022-10-26 21:51:46 -0300

    use auth routes and navbar config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,29 @@ import { Home } from './pages/Home/Home'
 import { Login } from './pages/Auth/Login'
 import { Register } from './pages/Auth/Register'
 
+// hooks
+import { useAuth } from './hooks/useAuth'
+
 // components
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 
 function App() {
+  const { auth, loading } = useAuth()
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route path='/' element={auth ? <Home /> : <Navigate to="/login" />} />
+            <Route path='/login' element={!auth ? <Login /> : <Navigate to="/" />} />
+            <Route path='/register' element={!auth ? <Register /> : <Navigate to="/" />} />
           </Routes>
         </div>
         <Footer />
