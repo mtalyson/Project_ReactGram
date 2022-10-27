d85a4994058155c26e545de820ea232bf6617846
Talyson Moreira <talyson.penha@gmail.com>
2022-10-26 22:39:18 -0300

    login done

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -9,14 +9,30 @@ import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 // redux
+import { login, reset } from '../../slices/authSlice'
 
 export const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const dispath = useDispatch()
+  const { loading, error } = useSelector((state) => state.auth)
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      email,
+      password
+    }
+
+    dispath(login(user))
   }
+
+  //clean all auth states
+  useEffect(() => {
+    dispath(reset())
+  }, dispath)
 
   return (
     <div id='login'>
@@ -25,7 +41,9 @@ export const Login = () => {
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email || ""} />
         <input type="password" placeholder='Senha' onChange={(e) => setPassword(e.target.value)} value={password || ""} />
-        <input type="submit" value="Entrar" />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
     </div>
