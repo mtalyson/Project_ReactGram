7f64da1b456fb8433bf2ea0cf4e76fb81fd5417a
Talyson Moreira <talyson.penha@gmail.com>
2022-10-26 22:16:16 -0300

    login page implemented

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,33 @@
 import './Auth.css'
 
+// components
+import { Link } from 'react-router-dom'
+import Message from '../../components/Message'
+
+// hooks
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+// redux
+
 export const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <div id='login'>
+      <h2>ReactGram</h2>
+      <p className='subtitle'>Faça o seu login para ver o que há de novo</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" placeholder='Senha' onChange={(e) => setEmail(e.target.value)} value={password || ""} />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
+    </div>
   )
 }
