929f6e60023ba65b085484b616f9d4cdda3262aa
Talyson Moreira <talyson.penha@gmail.com>
2022-11-21 16:35:46 -0300

    form to post a new photo created

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -13,9 +13,63 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 //redux
+import { getUserDetails } from '../../slices/userSlice'
 
 export const Profile = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+
+  const { user, loading } = useSelector((state) => state.user)
+  const { user: userAuth } = useSelector((state) => state.auth)
+
+  //new form and edit form refs
+  const newPhotoForm = useRef()
+  const editPhotoForm = useRef()
+
+  //load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id))
+  }, [dispatch, id])
+
+  const submitHandle = (e) => {
+    e.preventDefault()
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div>Profile</div>
+    <div id="profile">
+      <div className='profile-header'>
+        {user.profileImage && (
+          <img
+            src={`${uploads}/users/${user.profileImage}`}
+            alt={user.name} />
+        )}
+        <div className='profile-description'>
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+      {id === userAuth._id && (
+        <>
+          <div className='new-photo' ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Título para a foto:</span>
+                <input type="text" placeholder='Insira um título' />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" />
+              </label>
+              <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
