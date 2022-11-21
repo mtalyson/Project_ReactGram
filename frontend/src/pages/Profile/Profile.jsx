eb624d4d6daa858c6e2e270847422239e6244e9c
Talyson Moreira <talyson.penha@gmail.com>
2022-11-21 17:30:44 -0300

    insert new photos with form

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -14,6 +14,7 @@ import { useParams } from 'react-router-dom'
 
 //redux
 import { getUserDetails } from '../../slices/userSlice'
+import { publishPhoto, resetMessage } from '../../slices/photoSlice'
 
 export const Profile = () => {
   const { id } = useParams()
@@ -21,6 +22,10 @@ export const Profile = () => {
 
   const { user, loading } = useSelector((state) => state.user)
   const { user: userAuth } = useSelector((state) => state.auth)
+  const { photos, loading: loadingPhoto, message: messagePhoto, error: errorPhoto } = useSelector((state) => state.photo)
+
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
 
   //new form and edit form refs
   const newPhotoForm = useRef()
@@ -31,8 +36,34 @@ export const Profile = () => {
     dispatch(getUserDetails(id))
   }, [dispatch, id])
 
+  const handleFile = (e) => {
+    const image = e.target.files[0]
+    setImage(image)
+  }
+
   const submitHandle = (e) => {
     e.preventDefault()
+
+    const photoData = {
+      title,
+      image
+    }
+
+    //build form data
+    const formData = new FormData()
+    const photoFormData = Object.keys(photoData).forEach((key) => 
+      formData.append(key, photoData[key])
+    )
+
+    formData.append("photo", photoFormData)
+
+    dispatch(publishPhoto(formData))
+
+    setTitle("")
+
+    setTimeout(() => {
+      dispatch(resetMessage())
+    }, 2000)
   }
 
   if (loading) {
@@ -59,15 +90,18 @@ export const Profile = () => {
             <form onSubmit={submitHandle}>
               <label>
                 <span>Título para a foto:</span>
-                <input type="text" placeholder='Insira um título' />
+                <input type="text" placeholder='Insira um título' onChange={(e) => setTitle(e.target.value)} value={title || ""} />
               </label>
               <label>
                 <span>Imagem:</span>
-                <input type="file" />
+                <input type="file" onChange={handleFile} />
               </label>
-              <input type="submit" value="Postar" />
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto &&  <input type="submit" value="Aguarde..." disabled />}
             </form>
           </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
         </>
       )}
     </div>
