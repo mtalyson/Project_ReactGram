51dc422fad62a8da9723142461fa31d04beb77dc
Talyson Moreira <talyson.penha@gmail.com>
2022-11-21 17:38:07 -0300

    getUserPhotos on photoService created

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -15,8 +15,24 @@ const publishPhoto = async(data, token) => {
   }
 }
 
+//get user photos
+const getUserPhotos = async(id) => {
+  const config = requestConfig("GET")
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err)
+
+    return res
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const photoService = {
   publishPhoto,
+  getUserPhotos,
 }
 
 export default photoService
